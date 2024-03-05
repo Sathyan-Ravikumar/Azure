b7e936ed5e786635ef2833831ae1fd8720147a2f
Sathyan RaviKumar <sathyan.ravikumar@kanini.com>
2024-03-05 15:48:01 +0530

    deleted servicebus dir

--- ServiceBus/DuplicationDetection/DuplicationDetection.Config/AccountDetails.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DuplicationDetection.Config
-{
-    public class AccountDetails
-    {
-        public static string ConnectionString = "Endpoint=sb://simplemessagingusingservicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SfcZIuOGN1c1dtqrJHlLg5hQGyQCWs39s+ASbFnifXE=";
-        public static string QueueName = "duplication";
-    }
-}
