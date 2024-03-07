f1d657cd7d1c51fe318f8190f45731a38421eef1
Sathyan RaviKumar <sathyan.ravikumar@kanini.com>
2024-03-07 17:20:49 +0530

    change in swagger envionment

--- Blob Storage/BlobStorage/AzureBlobStorage/Program.cs
+++ Blob Storage/BlobStorage/AzureBlobStorage/Program.cs
@@ -1,6 +1,8 @@
 using Azure.Storage.Blobs;
 using AzureBlobStorage.Service;
 using AzureBlobStorage.Service.Interface;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Azure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +19,11 @@ builder.Services.AddScoped(_ =>
 {
     return new BlobServiceClient(builder.Configuration.GetConnectionString("AzureBlob"));
 });
+builder.Services.AddAzureClients(clientBuilder =>
+{
+    clientBuilder.AddBlobServiceClient(builder.Configuration["ConnectionStrings:AzureBlob:blob"], preferMsi: true);
+    clientBuilder.AddQueueServiceClient(builder.Configuration["ConnectionStrings:AzureBlob:queue"], preferMsi: true);
+});
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
@@ -25,6 +32,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
